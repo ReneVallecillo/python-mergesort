592db14e354cec2756d086bd71ec2fafb22f4a2f
Rene Vallecillo <reneval@gmail.com>
2013-07-12 16:40:08 -0600

    adding code

---
+++ mergesort.py
@@ -0,0 +1,43 @@
+with open('IntegerArray.txt', 'r') as f:
+	content = [int(line.rstrip('\r\n')) for line in f]
+	print("num of elements in document: " + str(len(content)))
+
+
+def merge_sort(unsorted_list):
+	size = len(unsorted_list)
+	if size <= 1:
+		return unsorted_list #consider it sorted
+
+	mid = int(size / 2)
+
+	left = unsorted_list[:mid]
+	right = unsorted_list[mid:]
+
+	left = merge_sort(left)
+	right = merge_sort(right)
+
+	return merge(left,right)
+
+def merge (left,right):
+	result = []
+	while len(left) > 0 or len(right) > 0: #mientras alguno de los dos tiene elementos
+		if len(left) > 0 and len(right) > 0:
+			if left[0] <= right[0]:
+				result.append(left[0])
+				left.remove(left[0])
+			else:
+				result.append(right[0])
+				right.remove(right[0])
+		elif len(left) > 0:
+			result += left
+			left = []
+		elif len(right) > 0:
+			result += right
+			right = []
+
+	return result
+
+
+
+data = merge_sort(content)
+print(data[:25])
